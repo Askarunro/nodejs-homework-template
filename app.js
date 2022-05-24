6f6508cc6dbb473a9bc0fa21e3f9ee090b27d93a
Askarunro <paskar.y.a@gmail.com>
2022-05-25 00:00:17 +0300

    Update app.js
    заміна шляху

--- app.js
+++ app.js
@@ -11,9 +11,9 @@ const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
 app.use(logger(formatsLogger))
 app.use(cors())
 app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
 
-app.use('/api/contacts', contactsRouter)
-
+app.use("/", contactsRouter);
 app.use((req, res) => {
   res.status(404).json({ message: 'Not found' })
 })
