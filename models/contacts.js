9a17ef75f87c14c0cefbc3a4a615124161e76540
Askarunro <paskar.y.a@gmail.com>
2022-05-25 00:00:20 +0300

    Update contacts.js

--- models/contacts.js
+++ models/contacts.js
@@ -1,14 +1,53 @@
-// const fs = require('fs/promises')
+const fs = require("fs/promises");
+const path = require("path");
+const contactsPath = path.join(__dirname, "./contacts.json");
 
-const listContacts = async () => {}
+const listContacts = async () => {
+  const readFile = await fs.readFile(contactsPath, "utf-8");
+  const contacts = JSON.parse(readFile);
+  return contacts;
+};
 
-const getContactById = async (contactId) => {}
+const getContactById = async (contactId) => {
+  const allContacts = await listContacts();
+  const contactById = allContacts.find((contact) => contact.id === contactId);
+  return contactById;
+};
 
-const removeContact = async (contactId) => {}
+const removeContact = async (contactId) => {
+  const allContacts = await listContacts();
+  const indexContactById = allContacts.findIndex(
+    (contact) => contact.id === contactId
+  );
+  if (indexContactById !== -1) {
+    allContacts.splice(indexContactById, 1);
+    return fs.writeFile(contactsPath, JSON.stringify(allContacts));
+  } else return undefined
+};
 
-const addContact = async (body) => {}
+const addContact = async (body) => {
+  const { name, email, phone } = body;
+  const allContacts = await listContacts();
+  if (name && email && phone) {
+    const newContact = { id: `${allContacts.length + 1}`, name, email, phone };
+    const newListContacts = [...allContacts, newContact];
+    fs.writeFile(contactsPath, JSON.stringify(newListContacts));
+    return listContacts();
+  } else return undefined;
+};
 
-const updateContact = async (contactId, body) => {}
+const updateContact = async (contactId, body) => {
+  const allContacts = await listContacts();
+  const indexContactById = allContacts.findIndex(
+    (contact) => contact.id === contactId
+  );
+  const contactById = allContacts.find((contact) => contact.id === contactId);
+  const renewContact = { ...contactById, ...body };
+  allContacts.splice(indexContactById, 1, renewContact);
+  fs.writeFile(contactsPath, JSON.stringify(allContacts));
+
+  return;
+};
 
 module.exports = {
   listContacts,
@@ -16,4 +55,4 @@ module.exports = {
   removeContact,
   addContact,
   updateContact,
-}
+};
