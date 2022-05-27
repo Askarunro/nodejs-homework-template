0d62e893da004fc43f9f87cc2fe3027c86930934
Askarunro <paskar.y.a@gmail.com>
2022-05-27 12:06:34 +0300

    Update app.js

--- app.js
+++ app.js
@@ -14,12 +14,6 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/", contactsRouter);
-app.use((req, res) => {
-  res.status(404).json({ message: 'Not found' })
-})
 
-app.use((err, req, res, next) => {
-  res.status(500).json({ message: err.message })
-})
 
 module.exports = app
