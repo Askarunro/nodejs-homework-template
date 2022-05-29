aa88a5b2942c6a68d18840c5363452e0c3d89128
Askarunro <paskar.y.a@gmail.com>
2022-05-29 21:32:37 +0300

    Update contacts.js

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,6 +1,12 @@
 const express = require("express");
-
+const Joi = require("joi");
 const router = express.Router();
+
+const schema = Joi.object({
+  name: Joi.string().min(3).max(30),
+  email: Joi.string().email(),
+  phone: Joi.string().min(13),
+});
 
 const {
   listContacts,
@@ -16,50 +22,56 @@ router.get("/", async (req, res, next) => {
 
 router.get("/:contactId", async (req, res, next) => {
   getContactById(req.params.contactId).then((value) => {
-    value
+    return value
       ? res.status(200).json(value)
       : res.status(404).json({ message: "Not found" });
   });
 });
 
 router.post("/", async (req, res, next) => {
-  addContact(req.body).then((value) =>
-    value
-      ? res.status(201).json(value)
-      : res.status(400).json({ message: "missing required name field" })
-  );
+  try {
+    if (Object.keys(req.body).length<=2 || schema.validate(req.body).error) {
+      return res.status(400).json({ message: "missing fields" });
+    }
+    addContact(req.body).then((value) => {
+      return res.status(201).json(value);
+    });
+  } catch {
+    res.status(500).json({ message: "Internal Server Error" });
+
+  }
 });
 
 router.delete("/:contactId", async (req, res, next) => {
-  removeContact(req.params.contactId).then(
-    (value) => {
-      if (!value) {
-        res.status(404).json({ message: "Not found" });
-      } else res.status(200).json({ message: "contact deleted" });
-    }
-
-    // value
-    //   ? res.status(200).json({ message: "contact deleted" })
-    //   : res.status(404).json({ message: "Not found" })
-  );
+  removeContact(req.params.contactId).then((value) => {
+    console.log(value);
+    return value
+      ? res.status(200).json({ message: "contact deleted" })
+      : res.status(404).json({ message: "Not found" });
+  });
 });
 
 router.put("/:contactId", async (req, res, next) => {
-  req.body
-    ? updateContact(req.params.contactId, req.body).then((value) => {
-        value
-          ? res.status(200).json(value)
-          : res.status(404).json({ message: "Not found" });
-      })
-    : res.status(400).json({ message: "missing fields" });
+  try {
+    if (Object.keys(req.body).length===0 || schema.validate(req.body).error) {
+      return res.status(400).json({ message: "missing fields" });
+    }
+    updateContact(req.params.contactId, req.body).then((value) => {
+      return value
+        ? res.status(200).json(value)
+        : res.status(404).json({ message: "Not found" });
+    });
+  } catch {
+    res.status(500).json({ message: "Internal Server Error" });
+  }
 });
 
 router.use((req, res) => {
-  res.status(404).json({ message: 'Not found' })
-})
+  res.status(404).json({ message: "Not found" });
+});
 
 router.use((err, req, res, next) => {
-  res.status(500).json({ message: err.message })
-})
+  res.status(500).json({ message: err.message });
+});
 
 module.exports = router;
