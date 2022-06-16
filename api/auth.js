8e1fe34184982fa8b11ed8669dbc68acd18d6427
Askarunro <paskar.y.a@gmail.com>
2022-06-16 20:01:38 +0300

    Update auth.js

--- api/auth.js
+++ api/auth.js
@@ -1,17 +1,17 @@
 const express = require("express");
 const router = express.Router();
-const { schemaRegister } = require("../service/schemas/users");
+const { schemaRegister, schemaLogin } = require("../service/schemas/users");
 const { validateRequest } = require("../middlewares/validateRequest");
 const { auth } = require("../middlewares/auth");
 
 const { registerUser, loginUser, logoutUser, currentUser } = require("../controller/auth");
 
-router.post("/register", registerUser);
+router.post("/register", validateRequest(schemaRegister), registerUser);
 
-router.post("/login", validateRequest(schemaRegister), loginUser);
+router.post("/login", validateRequest(schemaLogin), loginUser);
 
 router.post("/logout", auth, logoutUser);
 
-router.get("/current", currentUser);
+router.get("/current", auth, currentUser);
 
 module.exports = router;
